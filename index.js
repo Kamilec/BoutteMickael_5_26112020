7b4f69c14b22cdff7599b9fcabfe30945275d69f
Kamilec <70065389+Kamilec@users.noreply.github.com>
2020-12-17 16:52:24 +0100

    up 17/12 a

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 // URL de l'api
-const backendUrl = 'http://localhost:3000/api/cameras';
+const url = 'http://localhost:3000/api/cameras';
 
-async function getData(url) {
-  const res = await fetch(url);
-  const text = await res.text();
-  return text;
+// Affiche tous les produits
+const Products = async () => {
+  const products = await getAllCams(url);
+  products.forEach((product) => {
+    renderProduct(product.name, product._id, product.imageUrl, product.price);
+  });
+};
+// Récupère toutes les caméras
+const getAllCams = async (url) => {
+  const response = await fetch(url);
+  return await response.json();
+};
+
+// Fourni l'affichage d'un produit
+function renderProduct(productName, productId, productImg, productPrice) {
+
+  // Récupère la div qui contiendra les différents articles
+  const products = document.querySelector('#cameras');
+  const article = document.createElement('article');
+  article.innerHTML = `<img alt="${productName}" src="${productImg}">
+    <button class="product-link" type="button"><a href="product.html?id=${productId}">${productName} - ${
+    productPrice / 1000
+    }€</a></button>`;
+  
+  products.appendChild(article);
 }
 
-getData(backendUrl)
-  .then(function getProducts(result) {
-    const listP = JSON.parse(result);
-    for (let i = 0; i < listP.length; i++) {
-      console.log(listP[i]);
-
-      const productName = document.createElement('p');
-      productName.setAttribute('id', listP[i]._id);
-      productName.innerHTML = listP[i].name;
-
-      const myPrice = document.createElement('p');
-      myPrice.setAttribute('class', 'prix');
-      myPrice.innerHTML = listP[i].price;
-      productName.append(myPrice);
-
-      const img = document.createElement('img');
-      img.setAttribute('src', listP[i].imageUrl);
-      img.setAttribute('style', 'height: 50px; width: auto;');
-      productName.append(img);
-      console.log(productName);
-
-      const maDescription = document.createElement('p');
-      maDescription.setAttribute('class', 'description');
-      maDescription.innerHTML = listP[i].description;
-      productName.append(maDescription);
-
-      document.getElementById('products').append(productName);
-    }
-  })
-  .catch(function requestError(err) {
-    console.error(err);
-  });
+Products();
